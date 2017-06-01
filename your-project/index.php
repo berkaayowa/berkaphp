bc6d3dd0a083a9ae93adaafecb47354211727861
berkaayowa <ayowaberka@gmail.com>
2017-06-01 14:22:09 +0200

    fixed index

--- your-project/index.php
+++ your-project/index.php
@@ -1,14 +1,7 @@
 <?php
-
-    //echo phpinfo();
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-	//define('PREFIX', 'Admin', true);
-    //define('PREFIX', 'Customer', true);
 	require_once('AutoLoader.php');
 	use autoload\AppClassLoader;
 	AppClassLoader::loadIndexRequires();
 	use berkaPhp\config;
-	//$baseDir = 'http://'.$_SERVER['SERVER_NAME'];http://www.solutionsbg.com/software_development.php?lang=en
-	//http://fontawesome.io/icons/https://www.smartweb.co.za/web-hosting.php
+
 ?>
