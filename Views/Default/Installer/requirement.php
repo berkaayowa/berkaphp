f0661e7a19b254618db484475f727a6ede6df5cb
berkayongo <byayowa@sqli.com>
2017-07-18 01:02:35 +0200

    refactor AUTH class

---
+++ Views/Default/Installer/requirement.php
@@ -0,0 +1,26 @@
+<div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-12 col-xs-12">
+    <?= berkaPhp\helpers\Element::load("Requirement")?>
+    <div class="bs-callout bs-callout-warning" id="callout-formgroup-inputgroup">
+        <h4>Installation Requirements</h4>
+        <ul>
+            <li>Web Sever</li>
+            <li>Database MySQL</li>
+            <li>Copy of berkaPhp</li>
+        </ul>
+
+        <h4>Php version</h4>
+        <ul>
+            <li>Php version 5.6.0 or greater</li>
+        </ul>
+
+        <h4>Web server extensions that are required to be enabled</h4>
+        <ul>
+            <li>mod_rewrite extension shoud be enabled</li>
+            <li>mbstring PHP extension</li>
+            <li>intl PHP extension</li>
+            <li>curl PHP extension</li>
+        </ul>
+
+    </div>
+
+</div>
